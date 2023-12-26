bb782437e91f5c992f76a0d125c6ba957a067000
AllMid <dimagre15@gmail.com>
2023-12-26 05:57:55 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
+@pytest.fixture(scope="function")
+def browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--allow-running-insecure-content')
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
