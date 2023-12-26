7657352927f43be0910f586fff2d28970bf84a8e
AllMid <dimagre15@gmail.com>
2023-12-26 05:58:13 +0300

    Create GooglePages.py

---
+++ Pages/GooglePages.py
@@ -0,0 +1,32 @@
+from Pages.BaseTestOne import BasePage
+from selenium.webdriver.common.by import By
+
+class GoogleSeacrhLocators:
+    LOCATOR_GOOGLE_SEARCH_FIELD = (By.ID, "query")
+    LOCATOR_GOOGLE_SEARCH_BUTTON = (By.CLASS_NAME, "submit")
+    LOCATOR_GOOGLE_USERNAME_FIELD = (By.ID, "uid")
+    LOCATOR_GOOGLE_PASSWORD_FIELD = (By.ID, "passw")
+    LOCATOR_GOOGLE_LOGIN_BUTTON = (By.NAME, "btnSubmit")
+    LOCATOR_GOOGLE_MAIN_MESSAGE = (By.ID, "_ctl0__ctl0_Content_Main_message")
+
+class SearchHelper(BasePage):
+
+    def enter_word_username(self, word):
+        search_field = self.find_element(GoogleSeacrhLocators.LOCATOR_GOOGLE_USERNAME_FIELD)
+        search_field.click()
+        search_field.send_keys(word)
+        return search_field
+
+    def enter_word_password(self, word):
+        search_field = self.find_element(GoogleSeacrhLocators.LOCATOR_GOOGLE_PASSWORD_FIELD)
+        search_field.click()
+        search_field.send_keys(word)
+        return search_field
+
+    def click_on_the_login_button(self):
+        return self.find_element(GoogleSeacrhLocators.LOCATOR_GOOGLE_LOGIN_BUTTON,time=2).click()
+
+    def get_main_message(self):
+        return self.find_element(GoogleSeacrhLocators.LOCATOR_GOOGLE_MAIN_MESSAGE)
+
+    
